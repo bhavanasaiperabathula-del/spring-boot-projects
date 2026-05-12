88b207440e29ee7a2c9b1e5906eaaef6750364c1
bhavana_sai <chikita2sai@gmail.com>
2026-05-12 23:37:29 +0530

    Implemented basic security

---
+++ securityBasic/src/main/java/com/example/securityBasic/Service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.securityBasic.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.securityBasic.repository.UserDetailsRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDetailsRepository userDetailsRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    
+        return userDetailsRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    
+}
